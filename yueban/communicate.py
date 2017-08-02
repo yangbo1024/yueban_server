e6e233fee842d8f5858dfd79a477289ecbcdc2b8
杨波 <yangbo1024@qq.com>
2017-08-02 19:45:06 +0800

    up

--- yueban/communicate.py
+++ yueban/communicate.py
@@ -24,7 +24,7 @@ async def post(url, args):
 
 async def post_gater(gate_id, path, args):
     base_url = config.get_gate_url(gate_id)
-    url = '{0}/{1}'.format(base_url, path)
+    url = '{0}{1}'.format(base_url, path)
     data = utility.dumps(args)
     return await post(url, data)
 
@@ -38,34 +38,34 @@ async def post_all_gaters(path, args):
 
 async def post_logger(path, args):
     base_url = config.get_logger_url()
-    url = '{0}/{1}'.format(base_url, path)
+    url = '{0}{1}'.format(base_url, path)
     data = utility.dumps(args)
     return await post(url, data)
 
 
 async def post_scheduler(path, args):
     base_url = config.get_scheduler_url()
-    url = '{0}/{1}'.format(base_url, path)
+    url = '{0}{1}'.format(base_url, path)
     data = utility.dumps(args)
     return await post(url, data)
 
 
 async def post_game(path, args):
     base_url = config.get_game_url()
-    url = '{0}/{1}'.format(base_url, path)
+    url = '{0}{1}'.format(base_url, path)
     data = utility.dumps(args)
     return await post(url, data)
 
 
 async def post_ums(path, args):
     base_url = config.get_ums_url()
-    url = '{0}/{1}'.format(base_url, path)
+    url = '{0}{1}'.format(base_url, path)
     data = utility.dumps(args)
     return await post(url, data)
 
 
 async def post_cms(path, args):
     base_url = config.get_cms_url()
-    url = '{0}/{1}'.format(base_url, path)
+    url = '{0}{1}'.format(base_url, path)
     data = utility.dumps(args)
     return await post(url, data)
