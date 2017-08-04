d7a4c221f58ba5ad52483a13f6edc6d1dcd64947
杨波 <yangbo1024@qq.com>
2017-08-04 20:54:45 +0800

    up

--- yueban/logger.py
+++ yueban/logger.py
@@ -7,7 +7,6 @@ import logging
 import logging.handlers
 import os
 import os.path
-import filelock
 import time
 from logging.handlers import TimedRotatingFileHandler
 from aiohttp import web
@@ -27,7 +26,7 @@ def ensure_logger(category, log_name='yueban.log'):
     if not os.path.exists(category):
         os.makedirs(category)
     path = os.path.join(category, log_name)
-    handler = MultiProcessTimedRotatingFileHandler(path, when="midnight")
+    handler = MultiProcessTimedRotatingFileHandler(path, when="MIDNIGHT")
     handler.suffix = "%Y%m%d"
     handler.setFormatter(formatter)
     _logger.addHandler(handler)
@@ -57,14 +56,14 @@ class MultiProcessTimedRotatingFileHandler(TimedRotatingFileHandler):
     """
     def doRollover(self):
         """
-        do a rollover; in this case, a date/time stamp is appended to the filename
-        when the rollover happens.  However, you want the file to be named for the
-        start of the interval, not the current time.  If there is a backup count,
-        then we have to get a list of matching filenames, sort them and remove
-        the one with the oldest suffix.
+        do a rollover; in this case, a date/time stamp is appended to the
+        filename when the rollover happens.  However, you want the file to
+        be named for the start of the interval, not the current time.
+        If there is a backup count, then we have to get a list of matching
+        filenames, sort them and remove the one with the oldest suffix.
         """
-        #if self.stream:
-        #    self.stream.close()
+        if self.stream:
+            self.stream.close()
         # get the time that this sequence started at and make it a TimeTuple
         t = self.rolloverAt - self.interval
         if self.utc:
@@ -72,24 +71,12 @@ class MultiProcessTimedRotatingFileHandler(TimedRotatingFileHandler):
         else:
             timeTuple = time.localtime(t)
         dfn = self.baseFilename + "." + time.strftime(self.suffix, timeTuple)
-        #if os.path.exists(dfn):
-        #    os.remove(dfn)
-        with filelock.FileLock("rotate_yueban_log.lock"):
-            if not os.path.exists(dfn) and os.path.exists(self.baseFilename):
-                os.rename(self.baseFilename, dfn)
-                with open(self.baseFilename, 'a'):
-                    pass
+        if not os.path.exists(dfn):
+            os.rename(self.baseFilename, dfn)
         if self.backupCount > 0:
             # find the oldest log file and delete it
-            #s = glob.glob(self.baseFilename + ".20*")
-            #if len(s) > self.backupCount:
-            #    s.sort()
-            #    os.remove(s[0])
             for s in self.getFilesToDelete():
                 os.remove(s)
-        #print "%s -> %s" % (self.baseFilename, dfn)
-        if self.stream:
-            self.stream.close()
         self.mode = 'a'
         self.stream = self._open()
         currentTime = int(time.time())
@@ -97,13 +84,16 @@ class MultiProcessTimedRotatingFileHandler(TimedRotatingFileHandler):
         while newRolloverAt <= currentTime:
             newRolloverAt = newRolloverAt + self.interval
         #If DST changes and midnight or weekly rollover, adjust for this.
-        if (self.when == 'MIDNIGHT' or self.when.startswith('W')) and not self.utc:
+        if (self.when == 'MIDNIGHT' or self.when.startswith('W'))\
+                and not self.utc:
             dstNow = time.localtime(currentTime)[-1]
             dstAtRollover = time.localtime(newRolloverAt)[-1]
             if dstNow != dstAtRollover:
-                if not dstNow:  # DST kicks in before next rollover, so we need to deduct an hour
+             # DST kicks in before next rollover, so we need to deduct an hour
+                if not dstNow:
                     newRolloverAt = newRolloverAt - 3600
-                else:           # DST bows out before next rollover, so we need to add an hour
+             # DST bows out before next rollover, so we need to add an hour
+                else:
                     newRolloverAt = newRolloverAt + 3600
         self.rolloverAt = newRolloverAt
 
