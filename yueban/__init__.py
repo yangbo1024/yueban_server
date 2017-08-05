e2e90e413ee1c55b4dc724bebf17ba8a8c4ea7e2
杨波 <yangbo1024@qq.com>
2017-08-05 12:44:35 +0800

    update

--- yueban/__init__.py
+++ yueban/__init__.py
@@ -26,7 +26,7 @@ import json
 from . import config
 
 
-__version__ = '0.9.20'
+__version__ = '0.9.21'
 
 
 async def initialize(cfg):
--- yueban/log.py
+++ yueban/log.py
@@ -4,6 +4,7 @@
 Log functions
 """
 
+import asyncio
 from . import communicate
 
 
@@ -25,12 +26,24 @@ async def info(category, *args):
     await _log(category, 'INFO', *args)
 
 
+def info_async(category, *args):
+    asyncio.ensure_future(info(category, *args))
+
+
 async def warning(category, *args):
     await _log(category, 'WARNING', *args)
 
 
+def warning_async(category, *args):
+    asyncio.ensure_future(warning(category, *args))
+
+
 async def error(category, *args):
     await _log(category, 'ERROR', *args)
+
+
+def error_async(category, *args):
+    asyncio.ensure_future(error(category, *args))
 
 
 async def stat_many(collection_name, documents):
@@ -43,5 +56,13 @@ async def stat_many(collection_name, documents):
     await communicate.post_logger('/yueban/stat', [collection_name, documents])
 
 
+def stat_many_async(collection_name, documents):
+    asyncio.ensure_future(stat_many(collection_name, documents))
+
+
 async def stat(collection_name, document):
     await communicate.post_logger('/yueban/stat', [collection_name, [document]])
+
+
+def stat_async(collection_name, document):
+    asyncio.ensure_future(stat(collection_name, document))
