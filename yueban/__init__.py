988e0bc7b523e20b275889512bed2bdc70cb6ef7
快钱 <dwc23456@163.com>
2018-01-22 21:44:33 +0800

    up

--- yueban/__init__.py
+++ yueban/__init__.py
@@ -52,7 +52,7 @@ dig:
 from . import config
 
 
-__version__ = '2.2.3'
+__version__ = '2.2.4'
 
 
 async def initialize(cfg):
--- yueban/table.py
+++ yueban/table.py
@@ -15,7 +15,6 @@ import copy
 
 _cached_mtimes = {}
 _cached_tables = {}
-_inited = False
 
 
 def _get_table_path(table_name):
@@ -85,37 +84,40 @@ def update_table(table_name, table_data_str, encoding='utf-8'):
     _get_newest_table_data(table_name)
 
 
-def get_table(table_name):
+def get_table(table_name, clone=True):
     """
     获取整个表数据
     :param table_name:
+    :param clone:
     :return:
     """
     data = _get_newest_table_data(table_name)
-    return copy.deepcopy(data)
+    return copy.deepcopy(data) if clone else data
 
 
-def get_rows(table_name, index_name, index_value):
+def get_rows(table_name, index_name, index_value, clone=True):
     """
     获取能够匹配的所有行
     :param table_name:
     :param index_name:
     :param index_value:
+    :param clone:
     :return:
     """
     table_data = _get_newest_table_data(table_name)
     if not table_data:
         return None
-    ret = [copy.deepcopy(row_data) for row_data in table_data if row_data[index_name] == index_value]
-    return ret
+    ret = [row_data for row_data in table_data if row_data[index_name] == index_value]
+    return copy.deepcopy(ret) if clone else ret
 
 
-def get_row(table_name, index_name, index_value):
+def get_row(table_name, index_name, index_value, clone=True):
     """
     获取1行
     :param table_name:
     :param index_name:
     :param index_value:
+    :param clone:
     :return:
     """
     table_data = _get_newest_table_data(table_name)
@@ -123,23 +125,25 @@ def get_row(table_name, index_name, index_value):
         return None
     for row_data in table_data:
         if row_data[index_name] == index_value:
-            return copy.deepcopy(row_data)
+            return copy.deepcopy(row_data) if clone else row_data
     return None
 
 
-def get_cell(table_name, index_name, index_value, query_column):
+def get_cell(table_name, index_name, index_value, query_column, clone=True):
     """
     获取一个格子的内容
     :param table_name:
     :param index_name:
     :param index_value:
     :param query_column:
+    :param clone:
     :return:
     """
     row_map = get_row(table_name, index_name, index_value)
     if not row_map:
         return None
-    return copy.deepcopy(row_map.get(query_column))
+    cell = row_map.get(query_column)
+    return copy.deepcopy(cell) if clone else cell
 
 
 async def initialize():
